8c7101d67d05d84adee51dae30cd8c2f3267ed46
JinhanM <meijinhan0819@gmail.com>
2020-10-10 18:18:44 +0800

    Finish the game with single snake

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ import pygame
 import random
 
 
-
 mindist = 20
 size = 13
 dist = size * mindist + 100
@@ -51,6 +50,7 @@ class Snake:
         self.body = []
         self.empty_grid = [[i, j] for i in range(grids[0]) for j in range(grids[1])]
         self.food = None
+        self.last_move = [1, 0]
         self.create_body()
         self.create_food()
         self.timenoeat = 0
@@ -67,21 +67,22 @@ class Snake:
     def check_eaten(self, move):
         return [self.body[0][0] + move[0], self.body[0][1] + move[1]] == self.food
 
-    def move(self, move):
-        # 检查有没有吃到
-        if self.check_eaten(move):
-            self.body.insert(0, self.food)
-            self.create_food()
-            print(len(self.body))
-            print(len(self.empty_grid))
-        else:
-            self.empty_grid.append(self.body[-1])
-            self.body.pop(-1)
-            self.empty_grid.remove([self.body[0][0] + move[0], self.body[0][1] + move[1]])
-            self.body.insert(0, [self.body[0][0]+move[0], self.body[0][1]+move[1]])
-            print(self.body)
-            print(self.empty_grid)
+    def check_legit_move(self):
+        next_head = [self.body[0][0] + self.last_move[0], self.body[0][1] + self.last_move[1]]
+        return not (next_head[0] < 0 or next_head[1] < 0 or next_head[0] > grids[0]-1 or next_head[1] > grids[1]-1 or
+                    next_head in self.body)
 
+    def move(self):
+        if self.check_legit_move():
+            # 检查有没有吃到
+            if self.check_eaten(self.last_move):
+                self.body.insert(0, self.food)
+                self.create_food()
+            else:
+                self.empty_grid.append(self.body[-1])
+                self.body.pop(-1)
+                self.empty_grid.remove([self.body[0][0] + self.last_move[0], self.body[0][1] + self.last_move[1]])
+                self.body.insert(0, [self.body[0][0] + self.last_move[0], self.body[0][1] + self.last_move[1]])
 
     def draw(self, sc):
         pygame.draw.rect(sc, (255, 0, 0), ((self.food[0]*grid_size, (self.food[1])*grid_size), (grid_size, grid_size)))
@@ -89,6 +90,9 @@ class Snake:
                          (grid_size, grid_size)))
         for body in self.body[1:]:
             pygame.draw.rect(sc, (0, 100, 0), ((body[0]*grid_size, body[1]*grid_size), (grid_size, grid_size)))
+
+    def change_direction(self, next_move):
+        self.last_move = next_move
 
 
 class Game:
@@ -101,10 +105,12 @@ class Game:
         # 初始化screen
         pygame.init()
         pygame.font.init()
+        clock = pygame.time.Clock()
         # STAT_FONT = pygame.font.SysFont("comicsans", 50)
         screen = pygame.display.set_mode((WIDTH, HEIGHT))
         # 游戏开始
         while self._is_playing:
+            clock.tick(1)
             # 画背景
             screen.fill((255, 255, 255))
             # 画格子
@@ -115,25 +121,26 @@ class Game:
                 s.draw(screen)
 
             # 键盘操作
-            key_events = pygame.event.get(KEYDOWN)
             pygame.display.update()
-            for event in key_events:
-                if event.key in moves:
-                    snake.move(moves[event.key])
-                if event.key is K_ESCAPE:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
                     pygame.quit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        pygame.quit()
+                    if event.key in moves:
+                        snake.change_direction(moves[event.key])
+            if snake.check_legit_move():
+                snake.move()
+            else:
+                self._is_playing = False
+                print("Game Over")
 
 
 if __name__ == "__main__":
     snake = Snake()
     field = Field()
-    # field.draw()
-    # while True:
-    #     pygame.display.update()
-    #     key_events = pygame.event.get(KEYDOWN)
-    #     for event in key_events:
-    #         if event.key is K_ESCAPE:
-    #             pygame.quit()
 
     game = Game([field], [snake])
     game.run()
